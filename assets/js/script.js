cc723518a1fa234ca71bf17167e7edcbed768e5c
unknown <marisantos2305@hotmail.com>
2021-04-20 14:56:47 -0300

    Validação de assunto, botão de envio e zoom do mapa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 let nome = window.document.getElementById('nome')
 let email = document.querySelector("#email")
 let assunto = document.querySelector("#assunto")
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector("#mapa")
+
 
 nome.style.width = '100%'
 email.style.width = '100%'
@@ -13,6 +18,7 @@ if (nome.value.length < 3){
 } else{
     txtNome.innerHTML = "Nome válido"
     txtNome.style.color = 'green'
+    nomeOk = true
 }
 }
 
@@ -25,7 +31,37 @@ function validaEmail() {
     } else{
         txtEmail.innerHTML = "Email válido"
         txtEmail.style.color = 'green'
+        emailOk = true
     }
     }
 
-   
+    function validaAssunto () {
+        let txtAssunto = document.querySelector('#txtAssunto')
+    
+        if (assunto.value.length > 100) {
+            txtAssunto.innerHTML = 'Você ultrapassou o limite de caracteres'
+            txtAssunto.style.color = 'red'
+            txtAssunto.style.display = 'block'
+           
+        }else {
+            txtAssunto.style.display = 'none'
+            assuntoOk = true
+        }
+    }
+    
+    function enviar () {
+        if (nomeOk == true && emailOk == true && assuntoOk == true){
+        alert ("Enviado com sucesso!")}
+     else {
+        alert ('Ocorreu algum erro nos dados, preencha novamente') 
+    }
+} 
+function mapaZoom () {
+ mapa.style.width = '800px'
+ mapa.style.height = '600px'
+}
+
+function mapaNormal () {
+    mapa.style.width = '400px'
+    mapa.style.height = '250px'
+}
